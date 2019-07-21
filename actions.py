d3a2ac248f95233e4b965427f4696926fe2340c3
XAMES3 <xames3.developer@gmail.com>
2019-07-22 03:09:17 +0530

    Added: ActionPlayAnyMusic

--- actions.py
+++ actions.py
@@ -10,15 +10,21 @@ from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 
 from utils.functions.assistant import play_music
-from utils.profile.user import ai_lower, ai_title, lower, title
 
 
-class ActionPlayMusic(Action):
+class ActionPlayMusicWithTrackName(Action):
     def name(self):
-        return 'action_playing_music'
+        return 'action_play_music_with_track_name'
 
     def run(self, dispatcher, tracker, domain: dict) -> list:
-
         music = tracker.get_slot('track_name')
         dispatcher.utter_message(play_music(music))
         return [SlotSet('track_name', music)]
+
+
+class ActionPlayAnyMusic(Action):
+    def name(self):
+        return 'action_play_any_music'
+
+    def run(self, dispatcher, tracker, domain: dict) -> list:
+        dispatcher.utter_message(play_music())
