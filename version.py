925251e6316caa068b40c2414ddc7481f492f462
XAMES3 <xames3.developer@gmail.com>
2019-07-23 01:56:43 +0530

    Updated: doctring for the module and added support for using new directory structure

--- version.py
+++ version.py
@@ -1,27 +1,12 @@
 """
-Charlotte`s current version
-===========================
+Charlotte Version Module
+========================
 
-Returns current version of the package.
+This module returns the current version of Charlotte.
+Version data is retrieved from `setup.py` file.
 
-See https://github.com/xames3/charlotte for complete documentation.
+See https://github.com/xames3/charlotte for cloning the repository.
 """
-from utils.fluids.setup import VERSION
-
-# PEP0440 compatible formatted version, see:
-# https://www.python.org/dev/peps/pep-0440/
-#
-# Generic release markers:
-#   X.Y
-#   X.Y.Z   # For bugfix releases
-#
-# Admissible pre-release markers:
-#   X.YaN   # Alpha release
-#   X.YbN   # Beta release
-#   X.YrcN  # Release Candidate
-#   X.Y     # Final release
-#
-# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
-# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+from charlotte.setup import VERSION
 
 __version__ = VERSION
