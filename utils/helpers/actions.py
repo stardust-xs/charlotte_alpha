9cfb1ddeccf3e6e861921e3026b6d73311f9dee5
XAMES3 <xames3.developer@gmail.com>
2019-08-05 01:24:45 +0530

    Annexed: functions like current_weather and forecast_weather

--- utils/helpers/actions.py
+++ utils/helpers/actions.py
@@ -4,10 +4,13 @@ Charlotte Action Functions
 
 This module represents set of actions the Assistant can perform.
 The module has 4 functions:
-    - age          : Calculates the age
-    - locate       : Locates the position of the user
-    - find         : Finds the approximate `file name` in a directory
-    - play_music   : Plays music from `D:/Music` directory
+    - age               : Calculates the age
+    - locate            : Locates the position of the user
+    - find              : Finds the approximate `file name` in a directory
+    - play_music        : Plays music from `D:/Music` directory
+    - current_weather   : Returns current weather condition
+    - forecast_weather  : Returns weather forecast for next 5 hours
+    - current_forecast_weather: Returns current weather and forecast
 
 See https://github.com/xames3/charlotte for cloning the repository.
 """
@@ -177,3 +180,152 @@ def play_music(file: str = None, file_dir: str = local_dir['music']) -> None:
     except Exception as error:
         print('An error occured while performing this operation because of'
               f' {error}.')
+
+
+def current_weather(city: str) -> str:
+    import os
+    from random import choice
+    from apixu.client import ApixuClient
+
+    try:
+        client = ApixuClient(api_key=os.environ.get('CHARLOTTE_APIXU'))
+
+        local_area = client.current(q=city)
+        name = local_area['location']['name']
+        condition = local_area['current']['condition']['text']
+        temperature_c = local_area['current']['temp_c']
+        temperature_f = local_area['current']['temp_f']
+        is_day = local_area['current']['is_day']
+        wind_kph = local_area['current']['wind_kph']
+        wind_mph = local_area['current']['wind_mph']
+        wind_degree = local_area['current']['wind_degree']
+        wind_dir = local_area['current']['wind_dir']
+        pressure_in = local_area['current']['pressure_in']
+        pressure_mb = local_area['current']['pressure_mb']
+        precip_mm = local_area['current']['precip_mm']
+        precip_in = local_area['current']['precip_in']
+        humidity = local_area['current']['humidity']
+        cloud = local_area['current']['cloud']
+        feelslike_c = local_area['current']['feelslike_c']
+        feelslike_f = local_area['current']['feelslike_f']
+        vis_km = local_area['current']['vis_km']
+        vis_miles = local_area['current']['vis_miles']
+        uv = local_area['current']['uv']
+        gust_kph = local_area['current']['gust_kph']
+        gust_mph = local_area['current']['gust_mph']
+
+        day_weather = [f'Currently in {name} it\'s'
+                       f' {temperature_c}°C and {condition} with winds'
+                       f' running upto {wind_kph} km/h.',
+                       f'Right now in {name} it\'s {temperature_c}°C'
+                       f' and {condition} but because of the humidity'
+                       f' it feels like {feelslike_c}°C.',
+                       f'It is currently {condition} in {name} with '
+                       f' {temperature_c}°C and winds running upto {wind_kph}'
+                       ' km/h.',
+                       f'It is {temperature_c}°C and {condition} in {name}'
+                       f' with winds running upto {wind_kph} km/h but due to'
+                       f' the current humidity it feels like {feelslike_c}°C.',
+                       f'Weather in {name} is {temperature_c}°C'
+                       f' with {condition} conditions and winds blowing'
+                       f' in {wind_dir} direction.',
+                       f'The weather today in {name} is {condition}'
+                       f' with {temperature_c}°C.']
+        night_weather = [f'Tonight the weather is {condition} in {name}'
+                         f' with winds blowing in {wind_dir} direction at {wind_kph} km/h. Temperature is {temperature_c}°C.',
+                         f'Tonight the weather is {condition} in {name}'
+                         f' with {temperature_c}°C but due to {humidity}%'
+                         f' humidity it feels like {feelslike_c}°C.',
+                         f'The weather tonight in {name} is {condition}'
+                         f' with {temperature_c}°C.',
+                         f'Right now in {name} it\'s {temperature_c}°C'
+                         f' and {condition} but due to humidity'
+                         f' it feels like {feelslike_c}°C.',
+                         f'Currently in {name} it\'s {temperature_c}°C'
+                         f' and {condition} with winds running upto'
+                         f' {wind_kph} km/h.']
+
+        if is_day == 0:
+            return choice(night_weather)
+        else:
+            return choice(day_weather)
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error}.')
+
+
+def forecast_weather(city: str) -> str:
+    import os
+    from random import choice
+    from apixu.client import ApixuClient
+
+    try:
+        client = ApixuClient(api_key=os.environ.get('CHARLOTTE_APIXU'))
+
+        local_area = client.forecast(q=city, hour=5)
+        name = local_area['location']['name']
+        condition = local_area['current']['condition']['text']
+        temperature_c = local_area['current']['temp_c']
+        temperature_f = local_area['current']['temp_f']
+        is_day = local_area['current']['is_day']
+        wind_kph = local_area['current']['wind_kph']
+        wind_mph = local_area['current']['wind_mph']
+        wind_degree = local_area['current']['wind_degree']
+        wind_dir = local_area['current']['wind_dir']
+        pressure_in = local_area['current']['pressure_in']
+        pressure_mb = local_area['current']['pressure_mb']
+        precip_mm = local_area['current']['precip_mm']
+        precip_in = local_area['current']['precip_in']
+        humidity = local_area['current']['humidity']
+        cloud = local_area['current']['cloud']
+        feelslike_c = local_area['current']['feelslike_c']
+        feelslike_f = local_area['current']['feelslike_f']
+        vis_km = local_area['current']['vis_km']
+        vis_miles = local_area['current']['vis_miles']
+        uv = local_area['current']['uv']
+        gust_kph = local_area['current']['gust_kph']
+        gust_mph = local_area['current']['gust_mph']
+        maxtemp_c = local_area['forecast']['forecastday'][0]['day']['maxtemp_c']
+        maxtemp_f = local_area['forecast']['forecastday'][0]['day']['maxtemp_f']
+        mintemp_c = local_area['forecast']['forecastday'][0]['day']['mintemp_c']
+        mintemp_f = local_area['forecast']['forecastday'][0]['day']['mintemp_f']
+        avgtemp_c = local_area['forecast']['forecastday'][0]['day']['avgtemp_c']
+        avgtemp_f = local_area['forecast']['forecastday'][0]['day']['avgtemp_f']
+        maxwind_kph = local_area['forecast']['forecastday'][0]['day']['maxwind_kph']
+        maxwind_mph = local_area['forecast']['forecastday'][0]['day']['maxwind_mph']
+        totalprecip_mm = local_area['forecast']['forecastday'][0]['day']['totalprecip_mm']
+        totalprecip_in = local_area['forecast']['forecastday'][0]['day']['totalprecip_in']
+        avgvis_km = local_area['forecast']['forecastday'][0]['day']['avgvis_km']
+        avgvis_miles = local_area['forecast']['forecastday'][0]['day']['avgvis_miles']
+        avghumidity = local_area['forecast']['forecastday'][0]['day']['avghumidity']
+        forecast_condition = local_area['forecast']['forecastday'][0]['day']['condition']['text']
+        forecast_uv = local_area['forecast']['forecastday'][0]['day']['uv']
+
+        day_forecast = [f'Today, it will be {forecast_condition} with maximum'
+                        f' temperature of {maxtemp_c}°C and minimum of'
+                        f' {mintemp_c}°C.',
+                        'The temperature is predicted to be'
+                        f' {avgtemp_c}°C and with {forecast_condition}.',
+                        f'There will be winds blowing upto {maxwind_kph} km/h'
+                        ' and temperature would be anywhere'
+                        f' between {maxtemp_c}°C and {mintemp_c}°C.',
+                        f'It is forecasted to be about {avgtemp_c}°C with'
+                        f' {forecast_condition}.']
+        night_forecast = [f'Tonight the weather is predicted to be '
+                          f'{forecast_condition} with with maximum'
+                          f' temperature of {maxtemp_c}°C and minimum of'
+                          f' {mintemp_c}°C.',
+                          f'Tonight it is forecasted to be {avgtemp_c}°C with'
+                          f' {forecast_condition}.']
+
+        if is_day == 0:
+            return choice(night_forecast)
+        else:
+            return choice(day_forecast)
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error}.')
+
+
+def current_forecast_weather(city: str) -> str:
+    return current_weather(city) + ' ' + forecast_weather(city)
