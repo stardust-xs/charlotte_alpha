0e8f7832c6ed71222026a0a9a4c66d8b2afe4209
XAMES3 <xames3.developer@gmail.com>
2019-08-11 13:00:42 +0530

    Annexed: domain_builder function and docstrings for all functions

--- utils/helpers/rasa.py
+++ utils/helpers/rasa.py
@@ -5,7 +5,8 @@ Charlotte Rasa Functions
 This module provides reference to Rasa NLP toolkit.
 The module uses Rasa`s components and builds models, trains the model and
 test the model.
-The module has 4 functions:
+The module has 5 functions:
+    - domain_builder    : Updates domain file with user details
     - render_model      : Create models
     - run_nlu           : Runs NLU shell
     - start_training    : Starts Interactive Training process
@@ -18,22 +19,113 @@ from __future__ import print_function
 
 from os.path import exists
 from subprocess import call
-from yaml import load
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
 
 from charlotte.utils.helpers.common import display, make_dir, model_check
 from charlotte.utils.helpers.questions import answer, confirm
 from charlotte.utils.paths.directories import ai_dir
 from charlotte.utils.paths.files import ai_file
-from charlotte.utils.profiles.user import ai_lower, ai_title, lower, title
+from charlotte.utils.profiles.user import (age,
+                                           ai_title,
+                                           ai_lower,
+                                           lower,
+                                           title,
+                                           birthdate,
+                                           blood_type,
+                                           home_country,
+                                           email_primary,
+                                           email_secondary,
+                                           facebook_handle,
+                                           father_name,
+                                           first_name,
+                                           gender,
+                                           home_city,
+                                           instagram_handle,
+                                           last_name,
+                                           middle_name,
+                                           mother_name,
+                                           phone_primary,
+                                           phone_secondary,
+                                           skype_handle,
+                                           home_state,
+                                           home_street,
+                                           twitter_handle)
+
+
+def domain_builder() -> None:
+    """
+    Definition
+    ----------
+        Builds domain file with user details.
+
+    Notes
+    -----
+        The function builds the domain file from the values
+        which it imports from `./utils/profiles/user.py`.
+    """
+    from yaml import dump, load
+    try:
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Loader
+
+        domain = load(open(ai_file['domain']), Loader=Loader)
+        domain['slots']['xa']['initial_value'] = ai_title
+        domain['slots']['xa_age']['initial_value'] = age
+        domain['slots']['xa_birthdate']['initial_value'] = birthdate
+        domain['slots']['xa_blood_type']['initial_value'] = blood_type
+        domain['slots']['xa_home_country']['initial_value'] = home_country
+        domain['slots']['xa_email_primary']['initial_value'] = email_primary
+        domain['slots']['xa_email_secondary']['initial_value'] = email_secondary
+        domain['slots']['xa_facebook_handle']['initial_value'] = facebook_handle
+        domain['slots']['xa_father_name']['initial_value'] = father_name
+        domain['slots']['xa_first_name']['initial_value'] = first_name
+        domain['slots']['xa_gender']['initial_value'] = gender
+        domain['slots']['xa_home_city']['initial_value'] = home_city
+        domain['slots']['xa_instagram_handle']['initial_value'] = instagram_handle
+        domain['slots']['xa_last_name']['initial_value'] = last_name
+        domain['slots']['xa_middle_name']['initial_value'] = middle_name
+        domain['slots']['xa_mother_name']['initial_value'] = mother_name
+        domain['slots']['xa_phone_primary']['initial_value'] = phone_primary
+        domain['slots']['xa_phone_secondary']['initial_value'] = phone_secondary
+        domain['slots']['xa_skype_handle']['initial_value'] = skype_handle
+        domain['slots']['xa_home_state']['initial_value'] = home_state
+        domain['slots']['xa_home_street']['initial_value'] = home_street
+        domain['slots']['xa_twitter_handle']['initial_value'] = twitter_handle
+
+        with open(ai_file['domain'], 'w') as updated_domain:
+            updated_domain.write(dump(domain, default_flow_style=False))
+        with open(ai_file['domain'], 'r') as file_with_quoted_null:
+            newlines = []
+            for line in file_with_quoted_null.readlines():
+                newlines.append(line.replace("'null'", 'null'))
+        with open(ai_file['domain'], 'w') as file_without_quoted_null:
+            for line in newlines:
+                file_without_quoted_null.write(line)
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error}.')
 
 
 def render_model(model: str = None) -> None:
+    """
+    Definition
+    ----------
+        Renders model using command, `rasa train --fixed-model-name "<model>"`
+
+    Parameter
+    ---------
+        model : string, optional
+            Model to be created.
+            Global default: charlotte
+
+    Notes
+    -----
+        Function renders model by overwriting the previous model (if the model
+        has same name).
+    """
     try:
-        make_dir(ai_dir['models'])
+        domain_builder()
+        make_dir(ai_dir['models'], need_init=False)
         if model is None:
             option = confirm(
                 f'Would you like to rename the model, {lower}? Default name is "{ai_lower}".')
@@ -61,7 +153,16 @@ def render_model(model: str = None) -> None:
               f' {error}.')
 
 
-def run_nlu():
+def run_nlu() -> None:
+    """
+    Definition
+    ----------
+        Tests NLU model using command, `rasa shell nlu`.
+
+    Notes
+    -----
+        It will use the default Charlotte model, `./models/charlotte.tar.gz`
+    """
     try:
         if model_check(ai_lower):
             charlotte = model_check(ai_lower)
@@ -79,7 +180,17 @@ def run_nlu():
               f' {error}.')
 
 
-def start_ai_training():
+def start_ai_training() -> None:
+    """
+    Definition
+    ----------
+        Starts an Interactive Training session using command, `rasa
+        interactive`
+
+    Notes
+    -----
+        It will use the default Charlotte model, `./models/charlotte.tar.gz`
+    """
     try:
         if model_check(ai_lower):
             charlotte = model_check(ai_lower)
@@ -102,10 +213,16 @@ def nlu_stats() -> list:
     Definition
     ----------
         Returns all the intents and entities in NLU training data.
+        This values can be used for adding the intents and entities in the
+        domain file.
     """
     from rasa.nlu.training_data import load_data
 
-    nlu_data = load_data(str(ai_file['nlu']))
-    intents = [nlu_data.intents]
-    entities = [nlu_data.entities]
-    return intents, entities
+    try:
+        nlu_data = load_data(str(ai_file['nlu']))
+        intents = [nlu_data.intents]
+        entities = [nlu_data.entities]
+        return intents, entities
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error}.')
