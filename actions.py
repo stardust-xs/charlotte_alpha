40d7b1ae36211b62f84b23e86d2b77a4916e1427
XAMES3 <xames3.developer@gmail.com>
2019-08-11 12:39:21 +0530

    Annexed: support for using xa_home_city if no city is provided

--- actions.py
+++ actions.py
@@ -15,35 +15,19 @@ from charlotte.utils.helpers.actions import (play_music,
                                              current_forecast_weather)
 
 
-class ActionPlayMusicWithTrackName(Action):
-    def name(self):
-        return 'action_play_music_with_track_name'
-
-    def run(self, dispatcher, tracker, domain) -> list:
-        music = tracker.get_slot('track_name')
-        dispatcher.utter_message(play_music(music))
-        return [SlotSet('track_name', music)]
-
-
-class ActionPlayAnyMusic(Action):
-    def name(self):
-        return 'action_play_any_music'
-
-    def run(self, dispatcher, tracker, domain) -> list:
-        dispatcher.utter_message(play_music())
-
-
 class ActionTellCurrentWeatherConditions(Action):
     def name(self):
         return 'action_tell_current_weather_conditions'
 
     def run(self, dispatcher, tracker, domain) -> list:
         city = tracker.get_slot('city')
+        if city is None:
+            city = tracker.get_slot('xa_home_city')
         dispatcher.utter_message(current_weather(city))
         return [SlotSet('city', city)]
 
 
-class ActionTellForecaseWeatherConditions(Action):
+class ActionTellForecastWeatherConditions(Action):
     def name(self):
         return 'action_tell_forecast_weather_conditions'
 
@@ -51,8 +35,12 @@ class ActionTellForecaseWeatherConditions(Action):
         city = tracker.get_slot('city')
         hours = tracker.get_slot('hours')
         mins = tracker.get_slot('minutes')
+        if city is None:
+            city = tracker.get_slot('xa_home_city')
         dispatcher.utter_message(forecast_weather(city, hours, mins))
-        return [SlotSet('city', city), SlotSet('hours', hours), SlotSet('minutes', mins)]
+        return [SlotSet('city', city),
+                SlotSet('hours', hours),
+                SlotSet('minutes', mins)]
 
 
 class ActionTellCurrentForecastWeatherConditions(Action):
@@ -61,5 +49,7 @@ class ActionTellCurrentForecastWeatherConditions(Action):
 
     def run(self, dispatcher, tracker, domain) -> list:
         city = tracker.get_slot('city')
+        if city is None:
+            city = tracker.get_slot('xa_home_city')
         dispatcher.utter_message(current_forecast_weather(city))
         return [SlotSet('city', city)]
