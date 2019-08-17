b92e585d7c819b6b99571b7314882ca582020a16
XAMES3 <xames3.developer@gmail.com>
2019-08-17 23:29:05 +0530

    Annexed: new function for playing music, 'play_music_by_attribute'

--- utils/helpers/actions.py
+++ utils/helpers/actions.py
@@ -15,7 +15,10 @@ The module has 8 functions:
 
 See https://github.com/xames3/charlotte for cloning the repository.
 """
+from sys import exc_info
+
 from charlotte.utils.paths.directories import local_dir
+from charlotte.utils.paths.files import ai_file
 from charlotte.utils.profiles.user import title
 
 
@@ -51,7 +54,7 @@ def age(birthdate: str) -> int:
         return age
     except Exception as error:
         print('An error occured while performing this operation because of'
-              f' {error}.')
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
 
 
 def locate(area: str = None) -> str:
@@ -98,7 +101,7 @@ def locate(area: str = None) -> str:
                 return 'null'
     except Exception as error:
         print('An error occured while performing this operation because of'
-              f' {error}.')
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
 
 
 def find(file: str, file_dir: str, min_score: int = 65) -> tuple:
@@ -123,65 +126,28 @@ def find(file: str, file_dir: str, min_score: int = 65) -> tuple:
 
     Returns
     -------
-        actual_file[0], current_score : tuple, default
+        best_guess[0], current_score : tuple, default
             Returns correctly guessed file name with matching score.
     """
     from os import walk
-    from fuzzywuzzy import fuzz, process
+    from fuzzywuzzy import fuzz
+    from fuzzywuzzy.process import extract
 
     try:
         for root, _, files in walk(file_dir):
-            guessed_files = process.extract(
+            guessed_files = extract(
                 file, files, limit=3, scorer=fuzz.partial_ratio)
             no_match_score = 0
             no_match_found = f'Sorry, I could\'nt find \'{file}\' in the directory.'
-            for actual_file in guessed_files:
-                current_score = fuzz.partial_ratio(file, actual_file)
+            for best_guess in guessed_files:
+                current_score = fuzz.partial_ratio(file, best_guess)
                 if current_score > min_score and current_score > no_match_score:
-                    return actual_file[0], current_score
+                    return best_guess[0], current_score
                 else:
                     return no_match_found, no_match_score
     except Exception as error:
         print('An error occured while performing this operation because of'
-              f' {error}.')
-
-
-def play_music(file: str = None, file_dir: str = local_dir['music']) -> None:
-    """
-    Definition
-    ----------
-        Plays music from `D:/Music` directory.
-
-    Parameters
-    ----------
-        file : string, mandatory
-            Name of the file you need to search in the directory.
-            The name can be fuzzy.
-
-        file_dir : string, mandatory
-            Directory in which the file exists or needs to be searched in.
-            Here, it`s under Music directory.
-            Global default: D:/Music/
-    """
-    from os import startfile, listdir
-    from os.path import isfile, join
-    from random import choice
-
-    try:
-        if file is not None:
-            file_name, file_score = find(file, file_dir)
-            if file_score == 0:
-                return file_name
-            else:
-                music_file = join(file_dir, file_name)
-                startfile(f'{music_file}')
-        else:
-            random_file = choice([join(local_dir['music'], file) for file in listdir(
-                local_dir['music']) if isfile(join(local_dir['music'], file))])
-            startfile(random_file)
-    except Exception as error:
-        print('An error occured while performing this operation because of'
-              f' {error}.')
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
 
 
 def current_weather(city: str) -> str:
@@ -269,7 +235,7 @@ def current_weather(city: str) -> str:
             return choice(at_am)
     except Exception as error:
         print('An error occured while performing this operation because of'
-              f' {error}.')
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
 
 
 def forecast_weather(city: str, hours: int = None, mins: int = None) -> str:
@@ -401,7 +367,7 @@ def forecast_weather(city: str, hours: int = None, mins: int = None) -> str:
             return choice(with_hrs)
     except Exception as error:
         print('An error occured while performing this operation because of'
-              f' {error}.')
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
 
 
 def current_forecast_weather(city: str) -> str:
@@ -442,6 +408,189 @@ def wish_user() -> str:
     morning = choice([f'Good Morning, {title}.', 'Good Morning!'])
     afternoon = choice([f'Good Afternoon, {title}.', 'Good Afternoon!'])
     evening = choice([f'Good Evening, {title}.', 'Good Evening!'])
-    night = 'Night!'
+    night = f'Hello, {title}!'
     greeting = morning if hour >= 5 and hour < 12 else afternoon if hour >= 12 and hour < 17 else evening if hour >= 17 and hour < 22 else night
     return greeting
+
+
+def string_match(text: str, text_list: list, min_score: int = 65) -> tuple:
+    """
+    Definition
+    ----------
+        Finds the matching string in the list.
+
+    Parameters
+    ----------
+        text : string, mandatory
+            Name of the file you need to search in the directory.
+            The name can be fuzzy.
+
+        file_dir : string, mandatory
+            Directory in which the file exists or needs to be searched in.
+
+        min_score : integer, optional
+            Minimum score/Threshold score that should match while making as
+            approximate guess.
+            Global default: 65
+
+    Returns
+    -------
+        best_guess[0], current_score : tuple, default
+            Returns correctly guessed file name with matching score.
+    """
+    from os import walk
+    from fuzzywuzzy import fuzz
+    from fuzzywuzzy.process import extract
+
+    try:
+        if text is not None:
+            guessed = extract(text, text_list, limit=3,
+                              scorer=fuzz.partial_ratio)
+            no_match_score = 0
+            no_match_found = f'Sorry, I could\'nt find a matching \'{text}\'.'
+            for best_guess in guessed:
+                current_score = fuzz.partial_ratio(text, best_guess)
+                if current_score > min_score and current_score > no_match_score:
+                    return best_guess[0]
+                else:
+                    return no_match_found
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
+
+
+def play_music(file: str = None, file_dir: str = local_dir['music']) -> None:
+    """
+    Definition
+    ----------
+        Plays music from `D:/Music` directory.
+
+    Parameters
+    ----------
+        file : string, mandatory
+            Name of the file you need to search in the directory.
+            The name can be fuzzy.
+
+        file_dir : string, mandatory
+            Directory in which the file exists or needs to be searched in.
+            Here, it`s under Music directory.
+            Global default: D:/Music/
+    """
+    from os import startfile, listdir
+    from os.path import isfile, join
+    from random import choice
+
+    try:
+        if file is not None:
+            file_name, file_score = find(file, file_dir)
+            if file_score == 0:
+                return file_name
+            else:
+                music_file = join(file_dir, file_name)
+                startfile(f'{music_file}')
+        else:
+            music_file = choice([join(local_dir['music'], file) for file in listdir(
+                local_dir['music']) if isfile(join(local_dir['music'], file))])
+            startfile(music_file)
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
+
+
+def music_metadata_extractor(file: str) -> tuple:
+    """
+    Definition
+    ----------
+        Extracts the metadata from music files.
+
+    Parameters
+    ----------
+        file : string, mandatory
+            Name of the file you need to search in the directory.
+
+    Returns
+    -------
+        music_file : string, default
+        track_name : string, default
+        track_artist : string, default
+        track_albumartist : string, default
+        track_composer : string, default
+        track_album : string, default
+        track_genre : string, default
+        track_duration : string, default
+        track_year : string, default
+        track_filesize : string, default
+            Metadata of the music file.
+    """
+    from os.path import basename
+    from datetime import timedelta
+    from hurry.filesize import size, alternative
+    from tinytag import TinyTag
+
+    try:
+        music_track = TinyTag.get(file)
+        music_file = basename(file)
+        track_name = music_track.title
+        track_artist = music_track.artist
+        track_albumartist = music_track.albumartist
+        track_composer = music_track.composer
+        track_album = music_track.album
+        track_genre = music_track.genre
+        track_duration = str(timedelta(seconds=round(music_track.duration)))
+        track_year = music_track.year
+        track_filesize = size(music_track.filesize, system=alternative)
+
+        return music_file, track_name, track_artist, track_albumartist, track_composer, track_album, track_genre, track_duration, track_year, track_filesize
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
+
+
+def play_music_by_attribute(music_file: str = None,
+                            track_name: str = None,
+                            track_artist: str = None,
+                            track_albumartist: str = None,
+                            track_composer: str = None,
+                            track_album: str = None,
+                            track_genre: str = None,
+                            track_duration: str = None,
+                            track_year: str = None,
+                            track_filesize: str = None) -> None:
+    from random import randint
+    from numpy import ones
+    from pandas import read_csv, Series
+
+    try:
+        if music_file is not None:
+            play_music(music_file)
+        else:
+            music_csv_file = ai_file['music']
+            df = read_csv(music_csv_file, encoding='utf-8', )
+            master_dict = {
+                'track_name': string_match(track_name, df['track_name'].dropna().tolist()),
+                'track_artist': string_match(track_artist, df['track_artist'].dropna().tolist()),
+                'track_albumartist': string_match(track_albumartist, df['track_albumartist'].dropna().tolist()),
+                'track_composer': string_match(track_composer, df['track_composer'].dropna().tolist()),
+                'track_album': string_match(track_album, df['track_album'].dropna().tolist()),
+                'track_genre': string_match(track_genre, df['track_genre'].dropna().tolist()),
+                'track_duration': string_match(track_duration, df['track_duration'].dropna().tolist()),
+                'track_year': string_match(track_year, df['track_year'].dropna().tolist()),
+                'track_filesize': string_match(track_filesize, df['track_filesize'].dropna().tolist())}
+
+            filtered_dict = {k: v for k,
+                             v in master_dict.items() if v is not None}
+            filter_series = Series(ones(df.shape[0], dtype=bool))
+            for column, value in filtered_dict.items():
+                filter_series = ((df[column] == value) & filter_series)
+
+            if len(df[filter_series]) > 1:
+                random_pick = df[filter_series].iloc[randint(
+                    0, len(df[filter_series])), 0]
+                play_music(random_pick)
+            elif len(df[filter_series]) == 1:
+                play_music(df[filter_series].iloc[0, 0])
+            else:
+                return f'Sorry, {title}. I couldn\'t find any combination of the search parameters.'
+    except Exception as error:
+        print('An error occured while performing this operation because of'
+              f' {error} on line {exc_info()[-1].tb_lineno}.')
