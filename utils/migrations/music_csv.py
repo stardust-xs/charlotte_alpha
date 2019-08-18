472f648716f42e287194bfc0dd1c095db80a19c4
XAMES3 <xames3.developer@gmail.com>
2019-08-19 00:00:17 +0530

    Updated: simplified the code and added support for error handling

--- utils/migrations/music_csv.py
+++ utils/migrations/music_csv.py
@@ -5,57 +5,42 @@ Charlotte Music CSV Builder Module
 This module creates CSV of Music files and its metadata.
 
 The module encapsulates below metadata:
-    - track_name              : Original file name in local disk
-    - title                   : Original title of the music
-    - artist                  : Artist of the music
-    - albumartist             : Album artist OR Collaborator
-    - composer                : Composer of the music
-    - album                   : Album name
-    - genre                   : Genre of the music
-    - duration                : Duration of the music
-    - year                    : Release year of the music
-    - filesize                : Size of the music on local disk
-    - bitrate                 : Bitrate of the music
-    - samplerate              : Sample rate OR Frequency
-    - disc/track              : Track number
-    - disc_total/track_total  : Total tracks in the album
-    - comment                 : Comments
+    - music_file              : Original file name in local disk
+    - track_name              : Original title of the music
+    - track_artist            : Artist of the music
+    - track_albumartist       : Album artist OR Collaborator
+    - track_composer          : Composer of the music
+    - track_album             : Album name
+    - track_genre             : Genre of the music
+    - track_duration          : Duration of the music
+    - track_year              : Release year of the music
+    - track_filesize          : Size of the music on local disk
 
 See https://github.com/xames3/charlotte for cloning the repository.
 """
-from datetime import timedelta
-from os import walk
-from os.path import join
+from sys import exc_info
 
-from hurry.filesize import size, alternative
-
-from tinytag import TinyTag
-
-from charlotte.utils.helpers.common import csv_writer
 from charlotte.utils.paths.directories import local_dir
 from charlotte.utils.paths.files import ai_file
+from charlotte.utils.helpers.actions import music_metadata_extractor
+from charlotte.utils.helpers.common import csv_writer
 
-for _, _, files in walk(local_dir['music']):
-    for file in files:
-        music_file = TinyTag.get(join(local_dir['music'], file))
-        track_name = file
-        title = music_file.title
-        artist = music_file.artist
-        albumartist = music_file.albumartist
-        composer = music_file.composer
-        album = music_file.album
-        genre = music_file.genre
-        duration = str(timedelta(seconds=round(music_file.duration)))
-        year = music_file.year
-        filesize = size(music_file.filesize, system=alternative)
-        bitrate = music_file.bitrate
-        samplerate = music_file.samplerate
-        disc = music_file.disc
-        disc_total = music_file.disc_total
-        track = music_file.track
-        track_total = music_file.track_total
-        comment = music_file.comment
 
-        csv_writer(ai_file['music'], track_name, title, artist, albumartist,
-                   composer, album, genre, duration, year, filesize, bitrate,
-                   samplerate, disc, disc_total, track, track_total, comment)
+def migrate_music(file_dir: str = local_dir['music']) -> None:
+    from os import remove, walk
+    from os.path import isfile, join
+
+    header_list = ['music_file', 'track_name', 'track_artist',
+                   'track_albumartist', 'track_composer', 'track_album',
+                   'track_genre', 'track_duration', 'track_year',
+                   'track_filesize']
+    if isfile(ai_file['music']):
+        remove(ai_file['music'])
+    for _, _, files in walk(file_dir):
+        for file in files:
+            music_file, track_name, track_artist, track_albumartist, track_composer, track_album, track_genre, track_duration, track_year, track_filesize = music_metadata_extractor(
+                join(local_dir['music'], file))
+            csv_writer(ai_file['music'], music_file, track_name, track_artist,
+                       track_albumartist, track_composer, track_album,
+                       track_genre, track_duration, track_year,
+                       track_filesize)
