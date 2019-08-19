55e1ce47b894f7485d1e3b37fd3a7c4789ccc663
XAMES3 <xames3.developer@gmail.com>
2019-08-19 19:22:22 +0530

    Removed: link to key file

--- utils/paths/files.py
+++ utils/paths/files.py
@@ -1,7 +1,4 @@
 """
-Charlotte Files
-===============
-
 This module provides references to all necessary files.
 
 See https://github.com/xames3/charlotte for cloning the repository.
@@ -10,7 +7,6 @@ from charlotte.utils.paths.directories import ai_dir, local_dir, PARENT
 
 ai_file = {
     'init': '__init__.py',
-    'key': ai_dir['cryptics']/'key.xai',
     'nlu': ai_dir['data']/'nlu.md',
     'core': ai_dir['data']/'stories.md',
     'master': ai_dir['users']/'master.yml',
