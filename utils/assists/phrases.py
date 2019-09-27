c1728ca866fd2299a3d85f82f56d418d0727d004
XAMES3 <xames3.developer@gmail.com>
2019-09-27 17:19:56 +0530

    Updated: replaced all variable based phrases with dictionaries

--- utils/assists/phrases.py
+++ utils/assists/phrases.py
@@ -10,6 +10,9 @@ See https://github.com/xames3/charlotte for cloning the repository.
 #
 #   < Checkout my github repo for history and latest stable build >
 #
+#   1.0.6 - Replaced all options into respective dictionaries for better
+#           handling.
+#   1.0.5 - Updated `wish_user` to `greet_user`.
 #   1.0.4 - Added one example to `no_internet_connection` and fixed typo in it.
 #   1.0.3 - Added new phrase option, `no_internet_connection`.
 #   1.0.2 - Reduced unnecessary use of "`" in comments for simplicity.
@@ -19,70 +22,98 @@ from inspect import stack
 from random import choice
 from sys import exc_info
 
-from charlotte.utils.actions.person import wish_user
 from charlotte.utils.assists.profile import ai_lower, ai_title, lower, title
 
 try:
-    # All choices starting with `cmdline_main_options` are used by the run.py
-    # module.
-    cmdline_main_options_start_greet = choice([
-        wish_user() + ' How can I assist you?',
-        wish_user() + ' How can I help you?',
-        wish_user() + ' What would you like me to do?',
-        wish_user() + ' What can I help you with?',
-        wish_user() + ' What can I do for you?',
-        wish_user() + ' How may I help you?'])
-    cmdline_main_options_model_choice = choice([
-        'Which model you want to consider?',
-        f'Which model, {lower}?',
-        f'Alright. Which model, {lower}?',
-        f'Which model you want to use, {lower}?'])
-    cmdline_main_options_quit_confirm = choice([
-        'Are you sure about leaving?',
-        f'Are you sure, {lower}?',
-        f'Shall I terminate this session, {lower}?',
-        f'{title}, are you sure about this?',
-        f'Are you sure about this, {lower}?',
-        'Would you like me to close this session?'])
-    cmdline_main_options_clear_screen = choice([
-        'This will most likely clear everything on the screen. Shall I do it?',
-        f'{title}, are you sure you want me to clear the screen?',
-        f'Are you sure about this, {lower}?',
-        f'Am I supposed to clear everything, {lower}?',
-        'Would you like me to clear everything on the screen?'])
-    cmdline_main_options_user_command = choice([
-        f'{title}, would you like to try your own commands?',
-        f'Are you sure about this, {lower}?',
-        'Custom commands requires confirmation. Are you sure about this?',
-        'Would you like me to start another terminal session for you?'])
-    cmdline_main_options_terminal_set = choice([
-        f'The terminal is ready, {lower}.',
-        f'Your terminal is ready, {lower}.',
-        f'{title}, the terminal is ready. Please start typing.',
-        'Activating terminal... Ready'])
-    # Choices starting with `rasa_model` are used by the rasa.py module.
-    rasa_model_rename_check = choice([
-        f'Would you like to rename the model, {lower}? Default model name '
-        f'is "{ai_lower}".',
-        f'Do you wish to rename the model, {lower}? Default model name '
-        f'is "{ai_lower}".',
-        f'Would you like to rename the model, {lower}? "{ai_lower}" is '
-        'the default model name.',
-        f'Do you wish to rename the model, {lower}? "{ai_lower}" is '
-        'the default model name.',
-        f'Would you like me to rename the model, {lower}? "{ai_lower}" is '
-        'the default model name.'])
-    # Choices when no internet connection is detected.
-    no_internet_connection = choice([
-        f'{title}, it appears that we are not connected to the Internet. I am'
-        ' not able to perform the action.',
-        f'We have no internet connection right now, I am not able to perform'
-        ' the action.',
-        f'Working on it, {lower}. It seems the internet connection is lost.',
-        f'{title}, currently there is no internet connection available.',
-        f'{title}, the internet connection is questionable. I am not able'
-        ' to perform the action.',
-        f'{title}, I am not able to access the internet.'])
+    # For greetings.
+    greetings_protocol = {
+        'morning': [f'Good Morning, {title}.', 'Good Morning!'],
+        'afternoon': [f'Good Afternoon, {title}.', 'Good Afternoon!'],
+        'evening': [f'Good Evening, {title}.', 'Good Evening!'],
+        'night': [f'Hello, {title}!', f'Welcome back, {title}.'],
+        'yes': [f'{title}.', f'Yes, {lower}.']}
+    # For handling options on command prompt.
+    cmdline_options = {
+        'greetings': ['How can I assist you?',
+                      'How can I help you?',
+                      'What would you like me to do?',
+                      'What can I help you with?',
+                      'What can I do for you?',
+                      'How may I help you?'],
+        'choose_model': ['Which model you want to consider?',
+                         f'Which model, {lower}?',
+                         f'Alright. Which model, {lower}?',
+                         f'Which model you want to use, {lower}?'],
+        'confirm_quit': ['Are you sure about leaving?',
+                         f'Are you sure, {lower}?',
+                         f'Shall I terminate this session, {lower}?',
+                         f'{title}, are you sure about this?',
+                         f'Are you sure about this, {lower}?',
+                         'Would you like me to close this session?'],
+        'clear_screen': ['This will most likely clear everything on the'
+                         ' screen. Shall I do it?',
+                         f'{title}, are you sure you want me to clear the'
+                         ' screen?',
+                         f'Are you sure about this, {lower}?',
+                         f'Am I supposed to clear everything, {lower}?',
+                         'Would you like me to clear everything on the'
+                         ' screen?'],
+        'user_command': [f'{title}, would you like to try your own commands?',
+                         f'Are you sure about this, {lower}?',
+                         'Custom commands requires confirmation. Are you sure'
+                         ' about this?',
+                         'Would you like me to start another terminal '
+                         'session for you?'],
+        'terminal_set': [f'The terminal is ready, {lower}.',
+                         f'Your terminal is ready, {lower}.',
+                         f'{title}, the terminal is ready. Please start'
+                         ' typing.',
+                         'Activating terminal... Ready'],
+        'rename_model': [f'Would you like to rename the model, {lower}?'
+                         f' Default model name is "{ai_lower}".',
+                         f'Do you wish to rename the model, {lower}? Default'
+                         f' model name is "{ai_lower}".',
+                         f'Would you like to rename the model, {lower}?'
+                         f' "{ai_lower}" is the default model name.',
+                         f'Do you wish to rename the model, {lower}?'
+                         f' "{ai_lower}" is the default model name.',
+                         f'Would you like me to rename the model, {lower}?'
+                         f' "{ai_lower}" is the default model name.']}
+    # For handling errors.
+    errors = {
+        'no_internet': [f'{title}, it appears that we are not connected to'
+                        ' the Internet. I am not able to perform the action.',
+                        'We have no internet connection right now, I am not'
+                        ' able to perform the action.',
+                        f'Working on it, {lower}. It seems the internet'
+                        ' connection is lost.',
+                        f'{title}, currently there is no internet connection'
+                        ' available.',
+                        f'{title}, the internet connection is questionable.'
+                        ' I am not able to perform the action.',
+                        f'{title}, I am not able to access the internet.']}
+    # For handling weather related responses.
+    weather_protocol = {
+        'no_internet': [f'Internet connection is knackered, {lower}.',
+                        f'Internet connection is not good, {lower}.',
+                        f'Internet connection is questionable, {lower}.'],
+        'saying_okay': [f'Okay, {lower}.',
+                        f'Umm... okay, {lower}.',
+                        'Okay.',
+                        'On it.',
+                        'Erm... okay.'],
+        'working_on_it': ['Fetching weather details...',
+                          'Working on it.',
+                          'Just a moment...',
+                          'Processing...',
+                          'I\'m working on it...',
+                          'Accessing weather details...'],
+        'retrying': ['Not sure. I\'m working on it.',
+                     'Trying again...'],
+        'still_no_internet': [f'{title}, I\'m still not able to access the'
+                              ' internet.',
+                              'We still don\'t have a good internet'
+                              ' connection.']}
 except Exception as error:
     print('An error occured while performing this operation because of'
           f' {error} in function "{stack()[0][3]}" on line'
